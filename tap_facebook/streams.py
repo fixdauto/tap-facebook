b507dcffa88ffa9c68e5b3762a27e7b950641842
Bacotell36 <ryan@fixdapp.com>
2025-10-02 12:47:16 -0400

    add video_avg_time_watched_actions

--- tap_facebook/streams.py
+++ tap_facebook/streams.py
@@ -66,7 +66,7 @@ class AdsInsightsStream(FacebookStream):
         "video_p75_watched_actions",
         "video_p100_watched_actions",
         "video_thruplay_watched_actions",
-        "video_time_watched_actions",
+        "video_avg_time_watched_actions",
         "action_values",
     ]
 
@@ -160,7 +160,7 @@ class AdsInsightsStream(FacebookStream):
             ),
         ),
         Property(
-            "video_time_watched_actions",
+            "video_avg_time_watched_actions",
             ArrayType(
                 ObjectType(
                     Property("action_type", StringType),
